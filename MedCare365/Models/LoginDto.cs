c7b900b943a533437772b86d38bc6bc3188f0124
Ashish Autade <ashishautade1@gmail.com>
2025-10-04 12:46:06 +0530

    Login Dto and LoginSuccess View added in master

---
+++ MedCare365/Models/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedCare365.Models
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
